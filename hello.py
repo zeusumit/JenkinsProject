5e56074a1bc75d5969cef85ce33a608d1878a149
zeusumit <zeusumit@gmail.com>
2021-03-04 21:24:47 +0530

    1st commit

---
+++ hello.py
@@ -0,0 +1,46 @@
+print('Hello')
+print("Hello")
+print()
+print('This is an example of "Single and double"')
+print("This is an example of 'Single and double'")
+print("This is an example of \"Single and double\"")
+seperator='***'*5
+fruit="apple"
+print(fruit)
+print(fruit[0])
+print(fruit[3])
+fruit_len=len(fruit)
+print(fruit_len)
+print(len(fruit))
+print(fruit.upper())
+print('My'+''+'name'+''+'is'+''+'Sumit')
+print('My name is Sumit')
+first='sumit'
+second="Kumar"
+print(first+second)
+print(first+''+second)
+print(first+' '+second)
+fullname=first+' '+second
+print(fullname)
+print(fullname.upper())
+print(first[0].upper())
+print(first[0].upper()+first[1].lower())
+print('-'*10)
+happiness='happy '*3
+print(happiness)
+age=37
+print(first.upper()+"'"+'s'+' age is: '+str(age))
+print(seperator+'Formatting Strings'+seperator)
+print('My name is: {}'.format(first))
+print('My name is: {}'.format(first.upper()))
+print('{} is my name'.format(first))
+print('My name is {0} {1}. {1} {0} is my name'.format('Sumit', 'Kumar'))
+print('My name is {0} {1}. {1} {0} is my name'.format(first, second))
+print('{0:8} | {1:8}'. format(first, second))
+print('{0:8} | {1:0}'. format('Age', age))
+print('{0:8} | {1:8}'. format('Height', '6 feet'))
+print('{0:8} | {1:0} {2:1}'. format('Weight', 70,'kgs'))
+print(seperator+'User input'+seperator)
+
+
+
